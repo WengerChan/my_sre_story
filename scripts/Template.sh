3f999e4a162742bca830c652e0eebacd0900ad1a
WengerChan <37797982+WengerChan@users.noreply.github.com>
2021-02-08 12:23:23 +0800

    Add scripts
    add these scripts: add_swap_space.sh, check_nfs_service_status.sh, collect_cpu_mem_swap.sh, Template.sh

---
+++ scripts/Template.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+#=============================================
+# Script Name: xxxxxxxx
+#      Author: Wenger Chan
+#     Version: V 1.0
+#        Date: 2021-xx-xx
+#       Usage: sh xxxxx xxxx
+# Description: xxxxxxx
+#=============================================
+
+# Define variables -- BEGIN #
+# Define variables -- END #
+
+# Script Body -- BEGIN #
+# Script Body -- END #
